d59f912e4c0157f3a003bc6914a05a9aa530f098
qamefay <qamefay@8f2503c7-dd5a-0402-7408-2e9847ee7011>
2011-12-03 08:35:30 +0000

    帮助相关的Activity及一些样式修改,新增Activity自己在Manifest里声明

--- trunk/anroid/project/tax_client_by_android/src/cn/meitong/home/SMSSendActivity.java
+++ trunk/anroid/project/tax_client_by_android/src/cn/meitong/home/SMSSendActivity.java
@@ -2,7 +2,6 @@ package cn.meitong.home;
 
 import java.util.List;
 
-
 import android.app.Activity;
 import android.app.PendingIntent;
 import android.content.Intent;
@@ -19,40 +18,43 @@ import cn.meitong.listener.BackListener;
 import cn.meitong.listener.ButtonManager;
 
 public class SMSSendActivity extends Activity {
-	
-	private TextView sendToNo;
-	private Button smsSend;
-	private EditText message;
-	private TextView mTitle;
-	@Override
-	protected void onCreate(Bundle savedInstanceState) {
-		super.onCreate(savedInstanceState);
-		setContentView(R.layout.sms_send);
-		sendToNo = (TextView) findViewById(R.id.sendToNo);
-		mTitle = (TextView) findViewById(R.id.titleText);
-		mTitle.setText(R.string.title_sms_send);
-		new ButtonManager(this).getmBack().setOnClickListener(new BackListener(this));
-		smsSend = (Button) findViewById(R.id.sms_send);
-		smsSend.setOnClickListener(new OnClickListener() {
-			
-			public void onClick(View v) {
-				String strNo = sendToNo.getText().toString();
-                String strContent = message.getText().toString();
-                SmsManager smsManager = SmsManager.getDefault();
-                PendingIntent sentIntent = PendingIntent.getBroadcast(SMSSendActivity.this, 0, new Intent(), 0);
-               
-                if (strContent.length() > 70) {
-                    List<String> msgs = smsManager.divideMessage(strContent);
-                    for (String msg : msgs) {
-                        smsManager.sendTextMessage(strNo, null, msg, sentIntent, null);                        
-                    }
-                } else {
-                    smsManager.sendTextMessage(strNo, null, strContent, sentIntent, null);
-                }
-                Toast.makeText(SMSSendActivity.this, "以发送", Toast.LENGTH_LONG).show();
-            }
-		});
-		message = (EditText) findViewById(R.id.sms_message);
-		message.setText(getIntent().getStringExtra(SMSInputActivity.INTENE_MESSAGE));
-	}
+
+    private TextView sendToNo;
+    private Button smsSend;
+    private EditText message;
+    private TextView mTitle;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+	super.onCreate(savedInstanceState);
+	setContentView(R.layout.sms_send);
+	sendToNo = (TextView) findViewById(R.id.sendToNo);
+	mTitle = (TextView) findViewById(R.id.titleText);
+	mTitle.setText(R.string.title_sms_send);
+	new ButtonManager(this).getmBack().setOnClickListener(
+		new BackListener(this));
+	smsSend = (Button) findViewById(R.id.sms_send);
+	smsSend.setOnClickListener(new OnClickListener() {
+
+	    public void onClick(View v) {
+		String strNo = sendToNo.getText().toString();
+		String strContent = message.getText().toString();
+		SmsManager smsManager = SmsManager.getDefault();
+		PendingIntent sentIntent = PendingIntent.getBroadcast(
+			SMSSendActivity.this, 0, new Intent(), 0);
+		if (strContent != null) {
+		    smsManager.sendTextMessage(strNo, null, strContent,
+			    sentIntent, null);
+		    Toast.makeText(SMSSendActivity.this, strContent,
+			    Toast.LENGTH_LONG).show();
+		}else {
+		    Toast.makeText(SMSSendActivity.this, "不能为空",
+			    Toast.LENGTH_LONG).show();
+		}
+	    }
+	});
+	message = (EditText) findViewById(R.id.sms_message);
+	message.setText(getIntent().getStringExtra(
+		SMSInputActivity.INTENE_MESSAGE));
+    }
 }
