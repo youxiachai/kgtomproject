8dc9c52c564ecb9f096be331ddcdf0ceaf52982b
youxiachai <youxiachai@8f2503c7-dd5a-0402-7408-2e9847ee7011>
2011-12-03 13:12:41 +0000

    完成了一个比较大更新!!! 1,新增类可以在历史资源查看 2,log 的约定还是 soap 3,完成了,提供的 soap 报文构建,以及新增WSSE报文的支持,详细看注释

---
+++ anroid/project/tax_client_by_android/src/cn/meitong/model/RequestItem.java
@@ -0,0 +1,13 @@
+
+package cn.meitong.model;
+/**
+ * @author Tom_achai
+ * @date 2011-12-3
+ * 
+ */
+public class RequestItem {
+
+	public String fpdm;
+	public String fphm;
+	public String kpje;
+}
