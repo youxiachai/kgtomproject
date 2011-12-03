9a2fe20589da0192ffa39e0e2b59346c916a035d
chiefcto <chiefcto@8f2503c7-dd5a-0402-7408-2e9847ee7011>
2011-12-03 06:21:11 +0000

    修改了结果显示！！！

--- anroid/project/tax_client_by_android/src/cn/meitong/result/MyAdapter.java
+++ anroid/project/tax_client_by_android/src/cn/meitong/result/MyAdapter.java
@@ -31,7 +31,7 @@ public class MyAdapter extends ArrayAdapter<Invoice> {
 		
 		if(convertView == null) { 
             myLayout = new LinearLayout(getContext()); 
-            // ��һ��android�ĵ��й���LayoutInflater�Ķ���� 
+            // 锟斤拷一锟斤拷android锟侥碉拷锟叫癸拷锟斤拷LayoutInflater锟侥讹拷锟斤拷锟�
             // This class is used to instantiate layout XML file into its corresponding View objects. 
             // It is never be used directly -- use getLayoutInflater() or getSystemService(String) 
             // to retrieve a standard LayoutInflater instance that is already hooked up to the current 
@@ -39,19 +39,21 @@ public class MyAdapter extends ArrayAdapter<Invoice> {
             String inflater = Context.LAYOUT_INFLATER_SERVICE; 
             LayoutInflater vi = (LayoutInflater)getContext().getSystemService(inflater); 
             vi.inflate(res, myLayout, true); 
-            // ���ÿ��select imagesʱ�������� 
+            // 锟斤拷锟矫匡拷锟絪elect images时锟斤拷锟斤拷锟斤拷锟斤拷 
         } else { 
-            // ����ֻ�û�����ù� 
+            // 锟斤拷锟斤拷只锟矫伙拷锟斤拷锟斤拷霉锟�
             myLayout = (LinearLayout)convertView; 
         }
 		
-		TextView pnum = (TextView)myLayout.findViewById(R.id.num);
-		TextView id = (TextView)myLayout.findViewById(R.id.showrealut_Invoiceid);
-		TextView num = (TextView)myLayout.findViewById(R.id.showrealut_Invoicenum);
+		//TextView pnum = (TextView)myLayout.findViewById(R.id.num);
+		//TextView id = (TextView)myLayout.findViewById(R.id.showrealut_Invoiceid);
+		TextView title = (TextView)myLayout.findViewById(R.id.invoketitle);
+		TextView value = (TextView)myLayout.findViewById(R.id.invokevalue);
 		
-		pnum.setText("��¼"+String.valueOf(position)+"��");
-		id.setText(data.getInvoiceId());
-		num.setText(data.getInvoiceNum());
+//		pnum.setText("锟斤拷录"+String.valueOf(position)+"锟斤拷");
+//		id.setText(data.getInvoiceId());
+		title.setText(data.getInvoiceNum());
+		value.setText(data.get)
 		
 		return myLayout;
 	}
